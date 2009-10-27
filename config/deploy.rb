e023186bbfdd0e3bc6d1662674266b9fc8fead5c
Akshay Rawat <akshay.rawat@gmail.com>
2009-10-26 23:10:09 -0400

    corrections to deploy rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,34 @@
 set :application, "yourhires"
 set :deploy_to, "/var/www/#{application}"
 
+default_run_options[:pty] = true
+set :scm, :git
 set :repository, "git@github.com:akshayrawat/yourhires-ng.git"
 set :branch, "master"
-
 set :deploy_via, :remote_cache
 set :ssh_options, { :forward_agent => true }
-
-default_run_options[:pty] = true
 
 set :use_sudo, false
 set :user, "dev"
 set :domain, "yourhires.com"
-set :scm, :git
+set :default_env, 'production'
+set :rails_env, 'production'
 
 role :app, "www.yourhires.com"
 role :web, "www.yourhires.com"
 role :db,  "www.yourhires.com", :primary => true
-
-namespace :passenger do
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
 end
 
-after "deploy:setup", "db:setup" 
+def run_remote_rake(rake_cmd)
+    run "cd #{current_path} && rake RAILS_ENV=#{rails_env} #{rake_cmd}"
+end
+
+before "deploy:restart" do
+  run_remote_rake("db:setup")
+end
