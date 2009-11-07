5f03c8e0c6c8b45d687dd39940e9ce2329f1072f
Akshay Rawat <akshay.rawat@gmail.com>
2009-11-06 21:19:38 -0500

    validation messages on event create/update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,20 +7,20 @@ class EventsController < ApplicationController
 
     @events = upcoming_recruitment_steps.flatten.collect{|recruitment_step| recruitment_step.event}
   end
-  
+
   def new
     @candidate = Candidate.find(params[:candidate_id]) unless params[:candidate_id].blank?
   end
 
   def recruitment_steps
     render :nothing => true and return if params[:candidate_id].blank?
-    recruitment_steps = Candidate.find(params[:candidate_id]).recruitment_steps
-    render :partial => "recruitment_step", :collection => recruitment_steps
+    render :partial => "recruitment_step", 
+           :collection => Candidate.find(params[:candidate_id]).recruitment_steps
   end
 
   def create
     handle_action(Event.new(params[:event]), "Event was Created") do |event|
-      event.save(params[:event])
+      event.save
     end
   end
 
@@ -30,15 +30,19 @@ class EventsController < ApplicationController
     end
   end
 
-  def handle_action(event, message)
-    if yield(event)
-      render :update do |page|
-        @message= message
-        page.replace dom_id(event.recruitment_step), :partial => "recruitment_step", :locals => {:recruitment_step => event.recruitment_step, :message => @message}
-        page.visual_effect(:grow, "#{dom_id(event.recruitment_step.id)}_message")
+  private
+  
+  def handle_action(event, success_message)
+    render :update do |page|
+      if yield(event)
+        @message= success_message
+      else          
+        event.recruitment_step.event = event #HACK        
+        @message = "Please enter the highlighted fields"
       end
-    else
-      render :new
+      page.replace dom_id(event.recruitment_step), 
+              :partial => "recruitment_step", 
+              :locals => {:recruitment_step => event.recruitment_step, :message => @message}
     end
   end
 
