e77fe38353573181947eec5af48ef2bf44028101
Akshay Rawat <akshay.rawat@gmail.com>
2009-11-15 14:04:45 -0500

    tests tests tests

---
+++ spec/controllers/dashboard_controller_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe DashboardController do
+  integrate_views
+
+  before(:each) do
+    @maria = RecruiterFactory.maria
+    login_as @maria    
+  end
+
+  context "#index" do
+    it "should list first 5 upcoming events for the recruiter" do
+      candidate= CandidateFactory.create(:name => "Arnab", :recruiters => [@maria])
+
+      one = RecruitmentStepFactory.pairing(:event => EventFactory.create_in_past)
+      
+      two = RecruitmentStepFactory.interview(:event => EventFactory.create_in_future)
+      three = RecruitmentStepFactory.phone_interview(:event => EventFactory.create_in_future)
+      four = RecruitmentStepFactory.pairing(:event => EventFactory.create_in_future)
+      five = RecruitmentStepFactory.interview(:event => EventFactory.create_in_future)
+      six = RecruitmentStepFactory.phone_interview(:event => EventFactory.create_in_future)
+      seven = RecruitmentStepFactory.pairing(:event => EventFactory.create_in_future)
+
+      candidate.recruitment_steps = [one, two, three, four, five, six, seven]
+
+      get :index
+
+      assigns(:upcoming_events).should have(5).things
+
+      response.should_not have_tag("*[id=?]", "event_#{one.event.id}")
+      response.should have_tag("*[id=?]", "event_#{two.event.id}")
+      response.should have_tag("*[id=?]", "event_#{three.event.id}")
+      response.should have_tag("*[id=?]", "event_#{four.event.id}")
+      response.should have_tag("*[id=?]", "event_#{five.event.id}")
+      response.should have_tag("*[id=?]", "event_#{six.event.id}")
+      response.should_not have_tag("*[id=?]", "event_#{seven.event.id}")
+    end
+    
+    it "should show the event detail for the first event" do
+      candidate= CandidateFactory.create(:name => "Arnab", :recruiters => [@maria])
+      
+      one = RecruitmentStepFactory.pairing(:event => EventFactory.create_in_future)
+      two = RecruitmentStepFactory.interview(:event => EventFactory.create_in_future)
+      candidate.recruitment_steps = [one, two]
+
+      get :index
+      
+      response.should have_tag("*[id=?]", "detail_event_#{one.event.id}")
+    end
+  end
+
+end
--- spec/models/recruiter_spec.rb
+++ spec/models/recruiter_spec.rb
@@ -20,4 +20,16 @@ describe Recruiter do
     recruiter.should have(3).error_on(:login)
   end
   
+  it "should know upcoming events" do
+    maria = RecruiterFactory.create(:name => "Maria")
+    pairing = RecruitmentStepFactory.pairing(:event => EventFactory.create_in_past)
+    interview = RecruitmentStepFactory.interview(:event => EventFactory.create_in_future)
+
+    candidate= CandidateFactory.create(:name => "Arnab", 
+                :recruiters => [maria], :recruitment_steps => [pairing, interview])
+    
+    maria.upcoming_events.should have(1).thing            
+    maria.upcoming_events.first.recruitment_step.should eql(interview)
+  end
+  
 end
