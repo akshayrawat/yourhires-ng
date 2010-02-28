36d6ecd033611365b631db8ccbe25f6f7feb0fec
Akshay Rawat <projects@akshay.cc>
2010-02-28 18:24:17 -0500

    anonymizing seed data

--- spec/factories/recruiter_factory.rb
+++ spec/factories/recruiter_factory.rb
@@ -5,15 +5,15 @@ class RecruiterFactory
   end
 
   def self.valid_params
-    {:login => "maria_#{random_no}", :name => "Maria Anita", :email => "maria_#{random_no}@thoughtworks.com", :password => "yourhires", :password_confirmation => "yourhires"}
+    {:login => "maria_#{random_no}", :name => "Maria Anita", :email => "maria_#{random_no}@apple.com", :password => "yourhires", :password_confirmation => "yourhires"}
   end
   
   def self.maria
-    create(:login => "maria_#{random_no}", :name => "Maria Anita", :email => "maria_#{random_no}@thoughtworks.com")
+    create(:login => "maria_#{random_no}", :name => "Maria Anita", :email => "maria_#{random_no}@apple.com")
   end
   
   def self.reshmi
-    create(:login => "reshmi_#{random_no}", :name => "Reshmi Shenoy", :email => "reshmi_#{random_no}@thoughtworks.com")
+    create(:login => "reshmi_#{random_no}", :name => "Reshmi Shenoy", :email => "reshmi_#{random_no}@apple.com")
   end
 
 	def self.random_no
